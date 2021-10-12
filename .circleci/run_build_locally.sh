99897d22c7b6194f078e2aa04d2894f447ff5181
Miguel Mota <hello@miguelmota.com>
2021-10-12 11:35:29 -0700

    Update .circleci

--- .circleci/run_build_locally.sh
+++ .circleci/run_build_locally.sh
@@ -5,4 +5,4 @@ curl --user "${CIRCLE_TOKEN}:" \
      --form revision=0a042d26a7bdb34291c175d8603dbe8bfb21ad7b\
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/miguelmota/ipdr/tree/master
+        https://circleci.com/api/v1.1/project/github/ipdr/ipdr/tree/master
